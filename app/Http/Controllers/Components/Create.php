731ba19842941110f78123a4fa8296c300588e75
HoshmandG90 <hoshmandG900@gmail.com>
2021-09-29 16:14:26 +0300

    create some file

--- app/Http/Controllers/Components/Create.php
+++ app/Http/Controllers/Components/Create.php
@@ -1,13 +1,50 @@
 <?php
 
 namespace App\Http\Controllers\Components;
+use Livewire\WithPagination;
 
 use Livewire\Component;
-
+use App\Models\Records;
+use App\Models\Medicine;
 class Create extends Component
 {
+
+    public $patinent;
+    public $name;
+    public $types;
+    public $count;
+    public $notes;
+
+    public $ShowMedcinie=false;
+
+    public function StoreMedicine(){
+        $this->Validate([
+            'patinent' =>'required|string',
+            'name' =>'required',
+            'count' =>'required',
+            'types' =>'required',
+            'notes' =>'required'
+        ]);
+        Medicine::create([
+            'patinet_name'=>$this->patinent,
+            'name_medicine'=>$this->name,
+            'types'=>$this->types,
+            'count' =>$this->count,
+            'note'=>$this->notes,
+        ]);
+        $this->clear();
+
+    }
+
+    private function clear(){
+     $this->reset(['name','types','count','notes','patinent']);
+    }
     public function render()
     {
-        return view('components.create')->extends('layouts.app');
+        $records = Records::all();
+        $Medicines =Medicine::latest()->paginate(20);
+
+
+        return view('components.create',compact('records','Medicines'))->extends('layouts.app');
     }
 }
